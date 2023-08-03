24776648b009c713b2d6bbb2f2922ec45648d758
kevd4t <k.blanco@d4t.tech>
2023-08-03 10:28:08 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { Button } from "./components/Button";
 
@@ -8,7 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      <Button />
+      <Button>Shadcn</Button>
     </div>
   );
 }
